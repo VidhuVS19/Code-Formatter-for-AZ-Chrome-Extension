77bad592e2fffac3cf45dd13d5e7b442b827ed5f
VidhuVS19 <vidhu19122002@gmail.com>
2023-05-22 22:24:52 +0530

    6th commit

--- content.js
+++ content.js
@@ -7,17 +7,15 @@
 // License: MIT
 
 //<a _ngcontent-hib-c34="" class="ml-auto">
+//<a _ngcontent-ckx-c34="" class="btn btn-success submit_btn mr-1"><span _ngcontent-ckx-c34="" class="px-2">Submit </span></a>
 
-var parentElement = document.querySelector(".ml-auto")
-
-var divElement = document.createElement('div')
-
-var formatBTN = document.createElement('button')
-
+//const parentElement = document.querySelector(".ml-auto")
+const SubmitButton = document.querySelector(".submit_btn")
+const divElement = document.createElement('div')
+const formatBTN = document.createElement('button')
 formatBTN.style.borderRadius="10px"
-
+formatBTN.style.backgroundColor="yellow"
 formatBTN.textContent="Format"
-
-parentElement.appendChild(divElement)
 divElement.appendChild(formatBTN)
+SubmitButton.append(divElement)
 
